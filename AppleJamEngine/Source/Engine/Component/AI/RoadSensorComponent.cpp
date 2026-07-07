4acf467584d7064c647497e6dc971a5f1c6df3ed
김태현 <45249430+MozziDog@users.noreply.github.com>
2026-07-07 17:04:16 +0900

    feat: Add RoadSensorComponent

---
+++ AppleJamEngine/Source/Engine/Component/AI/RoadSensorComponent.cpp
@@ -0,0 +1,57 @@
+﻿#include "pch.h"
+#include "RoadSensorComponent.h"
+#include "GameFramework/World.h"
+#include "GameFramework/AActor.h"
+#include "Debug/DrawDebugHelpers.h"
+
+void URoadSensorComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	World = GetWorld();
+	BlackboardComp = Owner->GetComponentByClass<UBlackboardComponent>();
+	for (AActor* Actor : GetWorld()->GetActors())
+	{
+		URoadGraphComponent* Comp = Actor->GetComponentByClass<URoadGraphComponent>();
+		if (Comp)
+		{
+			RoadGraphComp = Comp;
+			break;
+		}
+	}
+	if (!RoadGraphComp.IsValid())
+	{
+		UE_LOG("[URoadSensorComponent] Cannot find URoadGraphComponent");
+	}
+}
+
+void URoadSensorComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction& ThisTickFunction)
+{
+	if (!RoadGraphComp.IsValid())
+	{
+		return;
+	}
+	
+	FRoadEdgeQueryResult Result = RoadGraphComp->GetRoadGraph().FindClosestPoint(GetWorldLocation());
+	FVector RoadLocation = Result.Position;
+	FVector RoadDirection = Result.Direction;
+
+	// '방향'은 무조건 Actor pivot으로부터 멀어지는 방향으로
+	FVector ComponentLocation = USceneComponent::GetRelativeLocation();
+	if (RoadDirection.Dot(ComponentLocation) < 0)
+	{
+		RoadDirection *= -1;
+	}
+
+	// Update Blackboard values
+	BlackboardComp->GetBlackboard().SetVector(BlackboardKey, RoadDirection);
+
+	// Debug draw
+	DrawDebugSphere(World, RoadLocation, 0.1f, 16, FColor::Red());
+	DrawDebugLine(World, RoadLocation, RoadLocation + RoadDirection * 3.0f, FColor::Red());
+}
+
+void URoadSensorComponent::ContributeSelectedVisuals(FScene& Scene) const
+{
+	Scene.AddDebugSphere(GetWorldLocation(), 0.3f, 16, FColor::Green());
+}
---
+++ AppleJamEngine/Source/Engine/Component/AI/RoadSensorComponent.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "Component/SceneComponent.h"
+#include "Component/AI/BlackboardComponent.h"
+#include "Component/AI/RoadGraphComponent.h"
+
+#include "Source/Engine/Component/AI/RoadSensorComponent.generated.h"
+
+UCLASS()
+class URoadSensorComponent : public USceneComponent
+{
+public:
+	GENERATED_BODY();
+	URoadSensorComponent() = default;
+
+	void BeginPlay() override;
+
+	// 선택된 액터의 컴포넌트에 대해 매 프레임 호출 — tick 없어도 에디터 상에서 프리뷰 표시
+	void ContributeSelectedVisuals(FScene& Scene) const override;
+
+protected:
+	void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction& ThisTickFunction) override;
+
+private:
+	UPROPERTY(Edit, Save, Category = "AI|Sensor")
+	FName BlackboardKey;
+	
+	TWeakObjectPtr<UWorld> World;	// Debug draw용
+	TWeakObjectPtr<UBlackboardComponent> BlackboardComp;
+	TWeakObjectPtr<URoadGraphComponent> RoadGraphComp;
+};
